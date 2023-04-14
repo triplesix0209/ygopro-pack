32520998a04094e54bfd2f5c117ffb1a6e4c7ec3
triplesix0209 <triplesix0209@gmail.com>
2023-04-14 14:58:30 +0700

    up

--- pack/divine-god/script/c900000001.lua
+++ pack/divine-god/script/c900000001.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
     e1:SetHintTiming(0, TIMING_BATTLE_END)
     e1:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
     e1:SetCondition(s.e1con)
-    e1:SetCost(s.e1cost)
+    e1:SetCost(s.effcost)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
@@ -31,21 +31,15 @@ function s.initial_effect(c)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
     e2:SetCondition(s.e2con)
-    e2:SetCost(s.e2cost)
+    e2:SetCost(s.effcost)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
     Utility.AvatarInfinity(s, c)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+function s.effcost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
-    local tn = Duel.GetTurnPlayer()
-    return Duel.GetCurrentChain(true) == 0 and (tn == tp and Duel.IsMainPhase()) or (tn ~= tp and Duel.IsBattlePhase()) and c:CanAttack()
-end
-
-function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) and c:GetAttackAnnouncedCount() == 0 end
+    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) and c:GetAttackAnnouncedCount() == 0 and c:CanAttack() end
 
     local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
     Duel.Release(g, REASON_COST)
@@ -57,6 +51,12 @@ function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     ec1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
     ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
     c:RegisterEffect(ec1)
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tn = Duel.GetTurnPlayer()
+    return Duel.GetCurrentChain(true) == 0 and (tn == tp and Duel.IsMainPhase()) or (tn ~= tp and Duel.IsBattlePhase())
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -89,14 +89,6 @@ function s.e2con(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     return Duel.IsExistingMatchingCard(function(tc) return tc:IsFaceup() and Divine.GetDivineHierarchy(tc) >= 2 end, tp, LOCATION_MZONE,
         LOCATION_MZONE, 1, nil) and c:GetBattleTarget() ~= nil and c:CanAttack()
-end
-
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) end
-
-    local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
-    Duel.Release(g, REASON_COST)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp) Utility.GainInfinityAtk(e:GetHandler(), RESET_PHASE + PHASE_DAMAGE_CAL) end
