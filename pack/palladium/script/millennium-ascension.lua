8554bc58a056dfa718db14797b58a18d3c6df5c8
triplesix0209 <triplesix0209@gmail.com>
2023-04-25 03:54:36 +0700

    up

--- pack/palladium/script/millennium-ascension.lua
+++ pack/palladium/script/millennium-ascension.lua
@@ -40,7 +40,7 @@ function s.initial_effect(c)
     e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE + EFFECT_FLAG_SET_AVAILABLE)
     e1:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
     e1:SetRange(LOCATION_FZONE)
-    e1:SetTarget(s.e1tg)
+    e1:SetTarget(function(e, c) return c:GetOwner() == e:GetOwnerPlayer() and c:IsSetCard(0x13a) end)
     c:RegisterEffect(e1)
     local e1b = e1:Clone()
     e1b:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
@@ -48,52 +48,64 @@ function s.initial_effect(c)
     local e1c = e1:Clone()
     e1c:SetCode(EFFECT_CANNOT_DISABLE_FLIP_SUMMON)
     c:RegisterEffect(e1c)
-    
-    -- "the true name" inactivatable
+
+    -- protect level 10 or higher monster
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_INACTIVATE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e2:SetRange(LOCATION_FZONE)
-    e2:SetValue(s.e2val)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(function(e, c) return c:IsLevelAbove(10) end)
+    e2:SetValue(1)
     c:RegisterEffect(e2)
     local e2b = e2:Clone()
-    e2b:SetCode(EFFECT_CANNOT_DISEFFECT)
+    e2b:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e2b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     c:RegisterEffect(e2b)
 
-    -- add "the true name"
+    -- "the true name" inactivatable
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 0))
-    e3:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_PREDRAW)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_INACTIVATE)
     e3:SetRange(LOCATION_FZONE)
-    e3:SetCondition(s.e3con)
-    e3:SetTarget(s.e3tg)
-    e3:SetOperation(s.e3op)
+    e3:SetValue(s.e3val)
     c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetCode(EFFECT_CANNOT_DISEFFECT)
+    c:RegisterEffect(e3b)
+
+    -- add "the true name"
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_PREDRAW)
+    e4:SetRange(LOCATION_FZONE)
+    e4:SetCondition(s.e4con)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
-function s.e1tg(e, tc) return tc:GetOwner() == e:GetOwnerPlayer() and tc:IsSetCard(0x13a) end
-
-function s.e2val(e, ct)
+function s.e3val(e, ct)
     local p = e:GetHandler():GetControler()
     local te, tp, loc = Duel.GetChainInfo(ct, CHAININFO_TRIGGERING_EFFECT, CHAININFO_TRIGGERING_PLAYER, CHAININFO_TRIGGERING_LOCATION)
     local tc = te:GetHandler()
     return p == tp and tc:IsCode(39913299) and (loc & LOCATION_ONFIELD) ~= 0
 end
 
-function s.e3filter(c) return c:IsCode(39913299) and c:IsAbleToHand() end
+function s.e4filter(c) return c:IsCode(39913299) and c:IsAbleToHand() end
 
-function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
     return tp == Duel.GetTurnPlayer() and Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 and Duel.GetDrawCount(tp) > 0
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil) end
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil) end
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK + LOCATION_GRAVE)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local dt = Duel.GetDrawCount(tp)
     if dt == 0 then return end
@@ -110,7 +122,7 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     Duel.RegisterEffect(ec1, tp)
     if _replace_count > _replace_max or not c:IsRelateToEffect(e) then return end
 
-    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, s.e3filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, s.e4filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
     if #g > 0 and Duel.SendtoHand(g, nil, REASON_EFFECT) > 0 then
         Duel.ConfirmCards(1 - tp, g)
         Duel.ShuffleHand(tp)
