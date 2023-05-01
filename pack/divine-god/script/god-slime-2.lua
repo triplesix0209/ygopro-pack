d3e2abbe7ee429f99650e23c5604a2de0faaa7db
triplesix0209 <triplesix0209@gmail.com>
2023-05-01 07:03:50 +0700

    up

---
+++ pack/divine-god/script/god-slime-2.lua
@@ -0,0 +1,79 @@
+-- Egyptian God Slime II
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- fusion summon
+    Fusion.AddProcMix(c, true, true, aux.FilterBoolFunctionEx(Card.IsRace, RACE_AQUA),
+        function(c, fc, sumtype, tp) return c:IsAttribute(ATTRIBUTE_WATER, fc, sumtype, tp) and c:GetLevel() == 10 end)
+
+    -- special summon
+    local sp = Effect.CreateEffect(c)
+    sp:SetType(EFFECT_TYPE_FIELD)
+    sp:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    sp:SetCode(EFFECT_SPSUMMON_PROC)
+    sp:SetRange(LOCATION_EXTRA)
+    sp:SetCondition(s.spcon)
+    sp:SetTarget(s.sptg)
+    sp:SetOperation(s.spop)
+    c:RegisterEffect(sp)
+
+    -- triple tribute
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_TRIPLE_TRIBUTE)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+
+    -- cannot be destroyed by battle
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    -- limit attack & effect activate
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_ATTACK)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTargetRange(0, LOCATION_MZONE)
+    e3:SetTarget(function(e, tc) return tc:GetAttack() <= e:GetHandler():GetAttack() end)
+    c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetCode(EFFECT_CANNOT_TRIGGER)
+    c:RegisterEffect(e3b)
+end
+
+function s.spfilter(c, tp, sc)
+    return c:IsRace(RACE_AQUA) and c:GetLevel() == 10 and c:GetAttack() == 0 and Duel.GetLocationCountFromEx(tp, tp, c, sc) > 0
+end
+
+function s.spcon(e, c)
+    if c == nil then return true end
+    local tp = c:GetControler()
+
+    return Duel.CheckReleaseGroup(tp, s.spfilter, 1, false, 1, true, c, tp, nil, nil, nil, tp, c)
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk, c)
+    local g = Duel.SelectReleaseGroup(tp, s.spfilter, 1, 1, false, true, true, c, tp, nil, false, nil, tp, c)
+    if g then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+        return true
+    end
+    return false
+end
+
+function s.spop(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+
+    Duel.Release(g, REASON_COST + REASON_MATERIAL)
+    g:DeleteGroup()
+end
