d120ff62306effe536183b7fcf83f8c6955c839a
triplesix0209 <triplesix0209@gmail.com>
2023-05-04 07:36:55 +0700

    up

--- pack/palladium/script/palladium-oracles.lua
+++ pack/palladium/script/palladium-oracles.lua
@@ -19,18 +19,6 @@ function s.initial_effect(c)
     splimit:SetValue(function(e, se, sp, st) return not e:GetHandler():IsLocation(LOCATION_EXTRA) or aux.fuslimit(e, se, sp, st) end)
     c:RegisterEffect(splimit)
 
-    -- add name
-    local addname = Effect.CreateEffect(c)
-    addname:SetType(EFFECT_TYPE_SINGLE)
-    addname:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
-    addname:SetCode(EFFECT_ADD_CODE)
-    addname:SetRange(LOCATION_MZONE)
-    addname:SetValue(71703785)
-    c:RegisterEffect(addname)
-    local addname2 = addname:Clone()
-    addname2:SetValue(42006475)
-    c:RegisterEffect(addname2)
-
     -- indes
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
