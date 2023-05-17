226161ae7a3db5c5503da95608fee0102a9b4e0b
triplesix0209 <triplesix0209@gmail.com>
2023-05-17 19:25:28 +0700

    update

--- pack/supreme-king-dragon/script/supreme-king-gate-hope.lua
+++ pack/supreme-king-dragon/script/supreme-king-gate-hope.lua
@@ -2,6 +2,36 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {13331639, 900005006}
+s.listed_series = {0x10f8}
+
 function s.initial_effect(c)
-	Pendulum.AddProcedure(c)
+    Pendulum.AddProcedure(c)
+
+    -- self destroy
+    local pe1 = Effect.CreateEffect(c)
+    pe1:SetType(EFFECT_TYPE_SINGLE)
+    pe1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    pe1:SetCode(EFFECT_SELF_DESTROY)
+    pe1:SetRange(LOCATION_PZONE)
+    pe1:SetCondition(s.pe1con)
+    c:RegisterEffect(pe1)
+
+    -- recover
+    local pe2 = Effect.CreateEffect(c)
+    pe2:SetCategory(CATEGORY_RECOVER)
+    pe2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    pe2:SetCode(900005006)
+    pe2:SetRange(LOCATION_PZONE)
+    pe2:SetOperation(s.pe2op)
+    c:RegisterEffect(pe2)
+end
+
+function s.pe1filter(c) return c:IsFaceup() and c:IsSetCard(0x10f8) end
+
+function s.pe1con(e) return not Duel.IsExistingMatchingCard(s.pe1filter, e:GetHandlerPlayer(), LOCATION_PZONE, 0, 1, e:GetHandler()) end
+
+function s.pe2op(e, tp, eg, ep, ev, re, r, rp)
+    Utility.HintCard(e)
+    Duel.Recover(tp, ev, REASON_EFFECT)
 end
--- pack/supreme-king-dragon/script/supreme-king-gate-sin.lua
+++ pack/supreme-king-dragon/script/supreme-king-gate-sin.lua
@@ -2,6 +2,51 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {13331639}
+
 function s.initial_effect(c)
     Pendulum.AddProcedure(c)
+
+    -- take no damage
+    local pe1 = Effect.CreateEffect(c)
+    pe1:SetType(EFFECT_TYPE_FIELD)
+    pe1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    pe1:SetCode(EFFECT_CHANGE_DAMAGE)
+    pe1:SetRange(LOCATION_PZONE)
+    pe1:SetTargetRange(1, 0)
+    pe1:SetLabel(0)
+    pe1:SetCondition(s.pe1con)
+    pe1:SetValue(s.pe1val)
+    c:RegisterEffect(pe1)
+    local pe1raise = Effect.CreateEffect(c)
+    pe1raise:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    pe1raise:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    pe1raise:SetCode(EVENT_ADJUST)
+    pe1raise:SetRange(LOCATION_PZONE)
+    pe1raise:SetLabelObject(pe1)
+    pe1raise:SetOperation(s.pe1op)
+    c:RegisterEffect(pe1raise)
+end
+
+function s.pe1filter(c) return c:IsFaceup() and c:IsCode(13331639) end
+
+function s.pe1con(e) return Duel.IsExistingMatchingCard(s.pe1filter, e:GetHandlerPlayer(), LOCATION_ONFIELD, 0, 1, nil) end
+
+function s.pe1val(e, re, val, r, rp, rc)
+    local tp = e:GetHandlerPlayer()
+    if val ~= 0 then
+        e:SetLabel(val)
+        return 0
+    else
+        return val
+    end
+end
+
+function s.pe1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local val = e:GetLabelObject():GetLabel()
+    if val ~= 0 then
+        Duel.RaiseEvent(c, id, e, REASON_EFFECT, tp, tp, val)
+        e:GetLabelObject():SetLabel(0)
+    end
 end
