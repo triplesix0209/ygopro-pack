567f6368714a76d809fc3e5fd29f0af54a2b9f87
lucthq-giganteast <lucthq@giganteast.com>
2023-06-04 01:29:06 +0700

    up

--- pack/supreme-king-dragon/script/astrochrono-sorcerer.lua
+++ pack/supreme-king-dragon/script/astrochrono-sorcerer.lua
@@ -6,5 +6,21 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     Pendulum.AddProcedure(c, false)
 
-    
+    -- place into pendulum zone
+    local me9 = Effect.CreateEffect(c)
+    me9:SetDescription(2203)
+    me9:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    me9:SetCode(EVENT_DESTROYED)
+    me9:SetProperty(EFFECT_FLAG_DELAY)
+    me9:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+        local c = e:GetHandler()
+        return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+    end)
+    me9:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk) if chk == 0 then return Duel.CheckPendulumZones(tp) end end)
+    me9:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
+        if not Duel.CheckPendulumZones(tp) then return end
+        local c = e:GetHandler()
+        if c:IsRelateToEffect(e) then Duel.MoveToField(c, tp, tp, LOCATION_PZONE, POS_FACEUP, true) end
+    end)
+    c:RegisterEffect(me9)
 end
--- pack/supreme-king-dragon/script/maiden-sky-iris.lua
+++ pack/supreme-king-dragon/script/maiden-sky-iris.lua
@@ -52,29 +52,58 @@ function s.initial_effect(c)
     me1check:SetLabelObject(me1)
     c:RegisterEffect(me1check)
 
-    -- place in pendulum zone
+    -- destroy replace
     local me2 = Effect.CreateEffect(c)
-    me2:SetDescription(aux.Stringid(id, 1))
-    me2:SetType(EFFECT_TYPE_QUICK_O)
-    me2:SetCode(EVENT_FREE_CHAIN)
+    me2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    me2:SetCode(EFFECT_DESTROY_REPLACE)
     me2:SetRange(LOCATION_MZONE)
-    me2:SetHintTiming(0, TIMING_END_PHASE)
-    me2:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
-    me2:SetCost(s.mecost)
     me2:SetTarget(s.me2tg)
-    me2:SetOperation(s.me2op)
-    c:RegisterEffect(me2, false, REGISTER_FLAG_DETACH_XMAT)
+    me2:SetValue(s.me2val)
+    c:RegisterEffect(me2)
+
+    -- place into pendulum zone (destroyed)
+    local me3 = Effect.CreateEffect(c)
+    me3:SetDescription(2203)
+    me3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    me3:SetCode(EVENT_DESTROYED)
+    me3:SetProperty(EFFECT_FLAG_DELAY)
+    me3:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+        local c = e:GetHandler()
+        return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+    end)
+    me3:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk) if chk == 0 then return Duel.CheckPendulumZones(tp) end end)
+    me3:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
+        if not Duel.CheckPendulumZones(tp) then return end
+        local c = e:GetHandler()
+        if c:IsRelateToEffect(e) then Duel.MoveToField(c, tp, tp, LOCATION_PZONE, POS_FACEUP, true) end
+    end)
+    c:RegisterEffect(me3)
+
+    -- place in pendulum zone (effect)
+    local me4 = Effect.CreateEffect(c)
+    me4:SetDescription(aux.Stringid(id, 1))
+    me4:SetType(EFFECT_TYPE_QUICK_O)
+    me4:SetCode(EVENT_FREE_CHAIN)
+    me4:SetRange(LOCATION_MZONE)
+    me4:SetHintTiming(0, TIMING_END_PHASE)
+    me4:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
+    me4:SetTarget(s.me4tg)
+    me4:SetOperation(s.me4op)
+    c:RegisterEffect(me4, false, REGISTER_FLAG_DETACH_XMAT)
 
     -- special summon from pendulum zone
-    local me3 = me2:Clone()
-    me3:SetDescription(aux.Stringid(id, 2))
-    me3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    me3:SetTarget(s.me3tg)
-    me3:SetOperation(s.me3op)
-    c:RegisterEffect(me3, false, REGISTER_FLAG_DETACH_XMAT)
+    local me5 = me4:Clone()
+    me5:SetDescription(aux.Stringid(id, 2))
+    me5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    me5:SetTarget(s.me5tg)
+    me5:SetOperation(s.me5op)
+    c:RegisterEffect(me5, false, REGISTER_FLAG_DETACH_XMAT)
 end
 
-function s.ovfilter(c, tp, sc) return c:IsFaceup() and c:IsType(TYPE_XYZ, sc, SUMMON_TYPE_XYZ) and c:IsSummonType(SUMMON_TYPE_PENDULUM) end
+function s.ovfilter(c, tp, sc)
+    return c:IsFaceup() and c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsType(TYPE_PENDULUM, sc, SUMMON_TYPE_XYZ) and
+               c:IsType(TYPE_XYZ, sc, SUMMON_TYPE_XYZ)
+end
 
 function s.pe1val(e, c)
     local tp = c:GetControler()
@@ -139,47 +168,64 @@ function s.me1op(e, tp, eg, ep, ev, re, r, rp)
     if #g > 0 then Duel.SSet(tp, g) end
 end
 
-function s.mecost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:CheckRemoveOverlayCard(tp, 1, REASON_COST) end
-
-    c:RemoveOverlayCard(tp, 1, 1, REASON_COST)
-    Duel.Hint(HINT_OPSELECTED, 1 - tp, e:GetDescription())
+function s.me2filter(c, tp)
+    return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD) and c:IsReason(REASON_BATTLE + REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
 end
 
-function s.me2filter(c) return c:IsFaceup() and c:IsType(TYPE_PENDULUM) end
+function s.me2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    local g = c:GetLinkedGroup()
+    if chk == 0 then return eg:IsExists(s.me2filter, 1, nil, tp) and c:CheckRemoveOverlayCard(tp, 1, REASON_EFFECT) end
 
-function s.me2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chk == 0 then
-        return (Duel.CheckLocation(tp, LOCATION_PZONE, 0) or Duel.CheckLocation(tp, LOCATION_PZONE, 1)) and
-                   Duel.IsExistingMatchingCard(s.me2filter, tp, LOCATION_MZONE, 0, 1, nil)
+    if Duel.SelectEffectYesNo(tp, c, 96) then
+        c:RemoveOverlayCard(tp, 1, 1, REASON_EFFECT)
+        return true
+    else
+        return false
     end
 end
 
-function s.me2op(e, tp, eg, ep, ev, re, r, rp)
+function s.me2val(e, c)
+    local tp = e:GetHandlerPlayer()
+    return s.me2filter(c, tp)
+end
+
+function s.me4filter(c) return c:IsFaceup() and c:IsType(TYPE_PENDULUM) end
+
+function s.me4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return (Duel.CheckLocation(tp, LOCATION_PZONE, 0) or Duel.CheckLocation(tp, LOCATION_PZONE, 1)) and
+                   Duel.IsExistingMatchingCard(s.me4filter, tp, LOCATION_MZONE, 0, 1, nil)
+    end
+
+    Duel.Hint(HINT_OPSELECTED, 1 - tp, e:GetDescription())
+end
+
+function s.me4op(e, tp, eg, ep, ev, re, r, rp)
     if not Duel.CheckLocation(tp, LOCATION_PZONE, 0) or not Duel.CheckLocation(tp, LOCATION_PZONE, 1) then return end
 
-    local g = Utility.SelectMatchingCard(aux.Stringid(id, 0), tp, s.me2filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
+    local g = Utility.SelectMatchingCard(aux.Stringid(id, 0), tp, s.me4filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
     Duel.HintSelection(g)
 
     if #g > 0 then Duel.MoveToField(g:GetFirst(), tp, tp, LOCATION_PZONE, POS_FACEUP, true) end
 end
 
-function s.me3filter(c, e, tp) return c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_PENDULUM, tp, false, false) end
+function s.me5filter(c, e, tp) return c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_PENDULUM, tp, false, false) end
 
-function s.me3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.me5tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
-        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and Duel.IsExistingMatchingCard(s.me3filter, tp, LOCATION_PZONE, 0, 1, nil, e, tp)
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and Duel.IsExistingMatchingCard(s.me5filter, tp, LOCATION_PZONE, 0, 1, nil, e, tp)
     end
 
+    Duel.Hint(HINT_OPSELECTED, 1 - tp, e:GetDescription())
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, 0, LOCATION_PZONE)
 end
 
-function s.me3op(e, tp, eg, ep, ev, re, r, rp)
+function s.me5op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
 
-    local g = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, s.me3filter, tp, LOCATION_PZONE, 0, 1, 1, nil, e, tp)
+    local g = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, s.me5filter, tp, LOCATION_PZONE, 0, 1, 1, nil, e, tp)
     Duel.HintSelection(g)
 
     if #g > 0 then Duel.SpecialSummon(g, SUMMON_TYPE_PENDULUM, tp, tp, false, false, POS_FACEUP) end
