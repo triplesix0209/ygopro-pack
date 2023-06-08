8ee1898da30c6d4711cad277cf716b448b7a5010
lucthq-giganteast <lucthq@giganteast.com>
2023-06-09 01:51:29 +0700

    up

--- pack/z-arc/script/supreme-king-servant-dragon-odd-eyes.lua
+++ pack/z-arc/script/supreme-king-servant-dragon-odd-eyes.lua
@@ -187,7 +187,7 @@ end
 function s.me2tg(e, c) return c:IsType(TYPE_PENDULUM) end
 
 function s.me2val(e, re, r, rp)
-    if ((r & REASON_BATTLE) ~= 0) or ((r & REASON_EFFECT) ~= 0 and re:GetHandlerPlayer() ~= e:GetHandlerPlayer()) then
+    if (r & REASON_BATTLE) ~= 0 or ((r & REASON_EFFECT) ~= 0 and re:GetHandlerPlayer() ~= e:GetHandlerPlayer()) then
         return 1
     else
         return 0
