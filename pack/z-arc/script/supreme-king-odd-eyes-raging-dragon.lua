859dc3556ba1cca8998d96fa730437fb19a50c2f
lucthq-giganteast <lucthq@giganteast.com>
2023-06-09 17:59:36 +0700

    up

--- pack/z-arc/script/supreme-king-odd-eyes-raging-dragon.lua
+++ pack/z-arc/script/supreme-king-odd-eyes-raging-dragon.lua
@@ -38,61 +38,76 @@ function s.initial_effect(c)
     pe1:SetOperation(s.pe1op)
     c:RegisterEffect(pe1)
 
-    -- place into pendulum zone
+    -- cannot be Tributed, or be used as a material
     local me1 = Effect.CreateEffect(c)
-    me1:SetDescription(2203)
-    me1:SetCategory(CATEGORY_DESTROY)
-    me1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    me1:SetCode(EVENT_DESTROYED)
-    me1:SetProperty(EFFECT_FLAG_DELAY)
-    me1:SetCondition(s.me1con)
-    me1:SetTarget(s.me1tg)
-    me1:SetOperation(s.me1op)
+    me1:SetType(EFFECT_TYPE_SINGLE)
+    me1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    me1:SetCode(EFFECT_UNRELEASABLE_SUM)
+    me1:SetRange(LOCATION_MZONE)
+    me1:SetValue(1)
     c:RegisterEffect(me1)
+    local me1b = me1:Clone()
+    me1b:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+    c:RegisterEffect(me1b)
+    local me1c = me1:Clone()
+    me1c:SetCode(EFFECT_CANNOT_TO_DECK)
+    c:RegisterEffect(me1c)
 
-    -- gain effect
+    -- place into pendulum zone
     local me2 = Effect.CreateEffect(c)
-    me2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    me2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    me2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    me2:SetDescription(2203)
+    me2:SetCategory(CATEGORY_DESTROY)
+    me2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    me2:SetCode(EVENT_DESTROYED)
+    me2:SetProperty(EFFECT_FLAG_DELAY)
     me2:SetCondition(s.me2con)
+    me2:SetTarget(s.me2tg)
     me2:SetOperation(s.me2op)
     c:RegisterEffect(me2)
-    local me2check = Effect.CreateEffect(c)
-    me2check:SetType(EFFECT_TYPE_SINGLE)
-    me2check:SetCode(EFFECT_MATERIAL_CHECK)
-    me2check:SetValue(s.me2check)
-    me2check:SetLabelObject(me2)
-    c:RegisterEffect(me2check)
+
+    -- gain effect
+    local me3 = Effect.CreateEffect(c)
+    me3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    me3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    me3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    me3:SetCondition(s.me3con)
+    me3:SetOperation(s.me3op)
+    c:RegisterEffect(me3)
+    local me3check = Effect.CreateEffect(c)
+    me3check:SetType(EFFECT_TYPE_SINGLE)
+    me3check:SetCode(EFFECT_MATERIAL_CHECK)
+    me3check:SetValue(s.me3check)
+    me3check:SetLabelObject(me3)
+    c:RegisterEffect(me3check)
 
     -- indes
-    local me3 = Effect.CreateEffect(c)
-    me3:SetType(EFFECT_TYPE_SINGLE)
-    me3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    me3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    me3:SetRange(LOCATION_MZONE)
-    me3:SetValue(s.effcon)
-    c:RegisterEffect(me3)
-
-    -- can make a second attack
     local me4 = Effect.CreateEffect(c)
     me4:SetType(EFFECT_TYPE_SINGLE)
-    me4:SetCode(EFFECT_EXTRA_ATTACK)
-    me4:SetCondition(s.effcon)
-    me4:SetValue(1)
+    me4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    me4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    me4:SetRange(LOCATION_MZONE)
+    me4:SetValue(s.effcon)
     c:RegisterEffect(me4)
 
-    -- negate & destroy
+    -- can make a second attack
     local me5 = Effect.CreateEffect(c)
-    me5:SetDescription(aux.Stringid(id, 2))
-    me5:SetCategory(CATEGORY_DESTROY)
-    me5:SetType(EFFECT_TYPE_IGNITION)
-    me5:SetRange(LOCATION_MZONE)
-    me5:SetCountLimit(1)
+    me5:SetType(EFFECT_TYPE_SINGLE)
+    me5:SetCode(EFFECT_EXTRA_ATTACK)
     me5:SetCondition(s.effcon)
-    me5:SetTarget(s.me5tg)
-    me5:SetOperation(s.me5op)
+    me5:SetValue(1)
     c:RegisterEffect(me5)
+
+    -- negate & destroy
+    local me6 = Effect.CreateEffect(c)
+    me6:SetDescription(aux.Stringid(id, 2))
+    me6:SetCategory(CATEGORY_DESTROY)
+    me6:SetType(EFFECT_TYPE_IGNITION)
+    me6:SetRange(LOCATION_MZONE)
+    me6:SetCountLimit(1)
+    me6:SetCondition(s.effcon)
+    me6:SetTarget(s.me6tg)
+    me6:SetOperation(s.me6op)
+    c:RegisterEffect(me6)
 end
 
 function s.lnkfilter(c, sc, sumtype, tp)
@@ -121,44 +136,44 @@ function s.pe1op(e, tp, eg, ep, ev, re, r, rp)
     if #g > 0 then Duel.MoveToField(g:GetFirst(), tp, tp, LOCATION_PZONE, POS_FACEUP, true) end
 end
 
-function s.me1con(e, tp, eg, ep, ev, re, r, rp)
+function s.me2con(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
 end
 
-function s.me1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.me2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local g = Duel.GetFieldGroup(tp, LOCATION_PZONE, 0)
     if chk == 0 then return #g > 0 end
 
     Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
 end
 
-function s.me1op(e, tp, eg, ep, ev, re, r, rp)
+function s.me2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local g = Duel.GetFieldGroup(tp, LOCATION_PZONE, 0)
     if Duel.Destroy(g, REASON_EFFECT) > 0 and c:IsRelateToEffect(e) then Duel.MoveToField(c, tp, tp, LOCATION_PZONE, POS_FACEUP, true) end
 end
 
-function s.me2checkfilter(c, sc) return c:IsType(TYPE_PENDULUM, sc, SUMMON_TYPE_LINK) and c:IsSummonType(SUMMON_TYPE_PENDULUM) end
+function s.me3checkfilter(c, sc) return c:IsType(TYPE_PENDULUM, sc, SUMMON_TYPE_LINK) and c:IsSummonType(SUMMON_TYPE_PENDULUM) end
 
-function s.me2check(e, c)
+function s.me3check(e, c)
     local g = c:GetMaterial()
-    if g:IsExists(s.me2checkfilter, 1, nil, c) then
+    if g:IsExists(s.me3checkfilter, 1, nil, c) then
         e:GetLabelObject():SetLabel(1)
     else
         e:GetLabelObject():SetLabel(0)
     end
 end
 
-function s.me2con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) and e:GetLabel() == 1 end
+function s.me3con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) and e:GetLabel() == 1 end
 
-function s.me2op(e, tp, eg, ep, ev, re, r, rp)
+function s.me3op(e, tp, eg, ep, ev, re, r, rp)
     e:GetHandler():RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD, EFFECT_FLAG_CLIENT_HINT, 1, 0, aux.Stringid(id, 1))
 end
 
 function s.effcon(e) return e:GetHandler():GetFlagEffect(id) > 0 end
 
-function s.me5tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.me6tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local ng = Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSpellTrap), tp, 0, LOCATION_ONFIELD, nil)
     local dg = Duel.GetFieldGroup(tp, 0, LOCATION_ONFIELD)
     if chk == 0 then return #dg > 0 end
@@ -167,7 +182,7 @@ function s.me5tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_DESTROY, dg, #dg, 0, 0)
 end
 
-function s.me5op(e, tp, eg, ep, ev, re, r, rp)
+function s.me6op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
 
     local ng = Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSpellTrap), tp, 0, LOCATION_ONFIELD, nil)
