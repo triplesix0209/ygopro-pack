231e000a982599ec43f8c385b3dd4359ec4684d3
lucthq-giganteast <lucthq@giganteast.com>
2023-06-16 00:15:40 +0700

    up

--- pack/divine-god/script/ra.lua
+++ pack/divine-god/script/ra.lua
@@ -8,6 +8,15 @@ s.listed_names = {95286165}
 function s.initial_effect(c)
     Divine.EgyptianGod(s, c, 2)
 
+    -- special summon limit
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE + EFFECT_FLAG_SINGLE_RANGE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    splimit:SetRange(LOCATION_HAND + LOCATION_DECK)
+    splimit:SetValue(aux.FALSE)
+    c:RegisterEffect(splimit)
+
     -- atk/def value
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
