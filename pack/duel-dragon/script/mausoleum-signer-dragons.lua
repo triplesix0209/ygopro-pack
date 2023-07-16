c9f43a881d170a13b981333df7f3e3f8446f69e2
lucthq-giganteast <lucthq@giganteast.com>
2023-07-17 06:24:14 +0700

    up

--- pack/duel-dragon/script/mausoleum-signer-dragons.lua
+++ pack/duel-dragon/script/mausoleum-signer-dragons.lua
@@ -54,6 +54,7 @@ function s.initial_effect(c)
 
     -- special summon
     local e5 = Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id, 2))
     e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e5:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
     e5:SetProperty(EFFECT_FLAG_DELAY)
