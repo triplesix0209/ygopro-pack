b60b7bb77359418dff004e5af293e8f063430541
lucthq-giganteast <lucthq@giganteast.com>
2023-07-20 00:10:13 +0700

    up

--- pack/duel-dragon/script/majestic-salvation.lua
+++ pack/duel-dragon/script/majestic-salvation.lua
@@ -18,13 +18,10 @@ function s.e1filter(c)
     if mt.synchro_tuner_required then ct = ct + mt.synchro_tuner_required end
     if mt.synchro_nt_required then ct = ct + mt.synchro_nt_required end
 
-    return c:IsFaceup() and c:IsLevelAbove(10) and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO) and ct > 0 and
-               c:GetFlagEffect(id) == 0
+    return c:IsFaceup() and c:IsLevelAbove(10) and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO) and ct > 0 and c:GetFlagEffect(id) == 0
 end
 
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_MZONE, 0, 1, nil) end
-end
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk) if chk == 0 then return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_MZONE, 0, 1, nil) end end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
@@ -54,36 +51,23 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     ec1c:SetCode(EFFECT_CANNOT_DISABLE)
     tc:RegisterEffect(ec1c)
 
-    -- indes
+    -- no return
     local ec2 = Effect.CreateEffect(c)
     ec2:SetType(EFFECT_TYPE_SINGLE)
     ec2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    ec2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
     ec2:SetRange(LOCATION_MZONE)
-    ec2:SetCountLimit(1)
-    ec2:SetValue(function(e, re, r, rp) return (r & REASON_EFFECT + REASON_BATTLE) ~= 0 end)
+    ec2:SetCode(EFFECT_CANNOT_TO_DECK)
     ec2:SetReset(RESET_EVENT + RESETS_STANDARD)
     tc:RegisterEffect(ec2)
 
-    -- atk/def up
+    -- untargetable
     local ec3 = Effect.CreateEffect(c)
-    ec3:SetDescription(aux.Stringid(id, 1))
-    ec3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    ec3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    ec3:SetDescription(3031)
+    ec3:SetType(EFFECT_TYPE_SINGLE)
+    ec3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    ec3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     ec3:SetRange(LOCATION_MZONE)
-    ec3:SetCondition(function(e, tp, eg, ep, ev, re, r, rp) return Duel.GetAttacker() == e:GetHandler() end)
-    ec3:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
-        local tc = e:GetHandler()
-        local ec1 = Effect.CreateEffect(c)
-        ec1:SetType(EFFECT_TYPE_SINGLE)
-        ec1:SetCode(EFFECT_UPDATE_ATTACK)
-        ec1:SetValue(1000)
-        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
-        tc:RegisterEffect(ec1)
-        local ec1b = ec1:Clone()
-        ec1b:SetCode(EFFECT_UPDATE_DEFENSE)
-        tc:RegisterEffect(ec1b)
-    end)
+    ec3:SetValue(aux.tgoval)
     ec3:SetReset(RESET_EVENT + RESETS_STANDARD)
     tc:RegisterEffect(ec3)
 end
