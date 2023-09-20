045658d12883cec045f4d097dd3e94efdf91e5c2
lucthq-giganteast <lucthq@giganteast.com>
2023-09-20 11:29:11 +0700

    up

---
+++ pack/neo-spacian-hero/script/elemental-hero-hellfire-neos.lua
@@ -0,0 +1,7 @@
+-- Elemental HERO Hellfire Neos
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
---
+++ pack/neo-spacian-hero/script/elemental-hero-nimbus-neos.lua
@@ -0,0 +1,7 @@
+-- Elemental HERO Nimbus Neos
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
