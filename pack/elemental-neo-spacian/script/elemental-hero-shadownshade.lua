cc3cc3b0c154a1521c3ea8b6c671be8b7f599ed0
lucthq-giganteast <lucthq@giganteast.com>
2023-10-05 18:57:35 +0700

    up

--- pack/elemental-neo-spacian/script/elemental-hero-shadownshade.lua
+++ pack/elemental-neo-spacian/script/elemental-hero-shadownshade.lua
@@ -2,6 +2,8 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_series = {SET_HERO, SET_ELEMENTAL_HERO}
+
 function s.initial_effect(c)
     -- normal monster
     local e1 = Effect.CreateEffect(c)
@@ -15,4 +17,102 @@ function s.initial_effect(c)
     e1b:SetCode(EFFECT_REMOVE_TYPE)
     e1b:SetValue(TYPE_EFFECT)
     c:RegisterEffect(e1b)
+
+    -- extra material
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCondition(function(e) return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), 69832741) end)
+    e2:SetOperation(Fusion.BanishMaterial)
+    e2:SetValue(function(e, c) return c:IsSetCard(SET_HERO) end)
+    c:RegisterEffect(e2)
+    
+    -- gain effect
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e3:SetProperty(EFFECT_FLAG_EVENT_PLAYER + EFFECT_FLAG_CANNOT_DISABLE)
+    e3:SetCode(EVENT_BE_MATERIAL)
+    e3:SetCondition(s.e3con)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+end
+
+function s.e3filter(c) return c:IsType(TYPE_FUSION) and c:IsSetCard(SET_ELEMENTAL_HERO) end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp) return eg:IsExists(s.e3filter, 1, nil) end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = eg:Filter(s.e3filter, nil, SET_HERO):GetFirst()
+    if not tc then return end
+
+    tc:RegisterFlagEffect(0, RESET_EVENT + RESETS_STANDARD, EFFECT_FLAG_CLIENT_HINT, 1, 0, aux.Stringid(id, 0))
+    if not tc:IsType(TYPE_EFFECT) then
+        local ec0 = Effect.CreateEffect(c)
+        ec0:SetType(EFFECT_TYPE_SINGLE)
+        ec0:SetCode(EFFECT_ADD_TYPE)
+        ec0:SetValue(TYPE_EFFECT)
+        ec0:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec0, true)
+    end
+
+    -- negate the effect
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(aux.Stringid(id, 1))
+    ec1:SetCategory(CATEGORY_DISABLE)
+    ec1:SetType(EFFECT_TYPE_QUICK_O)
+    ec1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    ec1:SetCode(EVENT_FREE_CHAIN)
+    ec1:SetRange(LOCATION_MZONE)
+    ec1:SetHintTiming(0, TIMINGS_CHECK_MONSTER)
+    ec1:SetCountLimit(1)
+    ec1:SetTarget(s.e3efftg)
+    ec1:SetOperation(s.e3effop)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+    tc:RegisterEffect(ec1, true)
+end
+
+function s.e3efftg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    if chk == 0 then return Duel.IsExistingTarget(Card.IsNegatable, tp, 0, LOCATION_MZONE, 1, c) end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_NEGATE)
+    local g = Duel.SelectTarget(tp, Card.IsNegatable, tp, 0, LOCATION_ONFIELD, 1, 1, c)
+    Duel.SetOperationInfo(0, CATEGORY_DISABLE, g, #g, 0, 0)
+end
+
+function s.e3effop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    if tc and ((tc:IsFaceup() and not tc:IsDisabled()) or tc:IsType(TYPE_TRAPMONSTER)) and tc:IsRelateToEffect(e) then
+        -- negate the effect
+        Duel.NegateRelatedChain(tc, RESET_TURN_SET)
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        ec1:SetCode(EFFECT_DISABLE)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        tc:RegisterEffect(ec1)
+        local ec1b = ec1:Clone()
+        ec1b:SetCode(EFFECT_DISABLE_EFFECT)
+        ec1b:SetValue(RESET_TURN_SET)
+        tc:RegisterEffect(ec1b)
+        if tc:IsType(TYPE_TRAPMONSTER) then
+            local ec1c = ec1:Clone()
+            ec1c:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+            tc:RegisterEffect(ec1c)
+        end
+
+        -- banish when leaves
+        local ec2 = Effect.CreateEffect(c)
+        ec2:SetDescription(3300)
+        ec2:SetType(EFFECT_TYPE_SINGLE)
+        ec2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
+        ec2:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+        ec2:SetValue(LOCATION_REMOVED)
+        ec2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        tc:RegisterEffect(ec2)
+    end
 end
