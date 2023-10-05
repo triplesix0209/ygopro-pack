d5fe4a37ee134d7ef07e967152d0fd6938267b9d
lucthq-giganteast <lucthq@giganteast.com>
2023-10-05 17:06:52 +0700

    up

---
+++ pack/elemental-neo-spacian/script/elemental-hero-thunderman.lua
@@ -0,0 +1,18 @@
+-- Elemental HERO Thunderman
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    -- normal monster
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_ADD_TYPE)
+    e1:SetRange(LOCATION_HAND + LOCATION_GRAVE + LOCATION_ONFIELD)
+    e1:SetValue(TYPE_NORMAL)
+    c:RegisterEffect(e1)
+    local e1b = e1:Clone()
+    e1b:SetCode(EFFECT_REMOVE_TYPE)
+    e1b:SetValue(TYPE_EFFECT)
+    c:RegisterEffect(e1b)
+end
