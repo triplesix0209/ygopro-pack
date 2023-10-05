81159ddc3c595cd4a32d99a1043ed9a69be4915b
lucthq-giganteast <lucthq@giganteast.com>
2023-10-05 17:46:25 +0700

    up

--- pack/elemental-neo-spacian/script/elemental-hero-wingman.lua
+++ pack/elemental-neo-spacian/script/elemental-hero-wingman.lua
@@ -1,8 +1,7 @@
--- Elemental HERO Blazinatrix
+-- Elemental HERO Wingman
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.listed_names = {58932615}
 s.listed_series = {SET_HERO, SET_ELEMENTAL_HERO}
 
 function s.initial_effect(c)
@@ -19,12 +18,11 @@ function s.initial_effect(c)
     e1b:SetValue(TYPE_EFFECT)
     c:RegisterEffect(e1b)
 
-    -- special summon
+    -- special summon itself from hand or GY
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_MZONE)
+    e2:SetRange(LOCATION_HAND + LOCATION_GRAVE)
     e2:SetCountLimit(1, id)
     e2:SetCost(s.e2cost)
     e2:SetTarget(s.e2tg)
@@ -41,29 +39,33 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
-function s.e2filter(c, e, tp) return not c:IsCode(58932615) and c:IsSetCard(SET_HERO) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+function s.e2filter(c) return c:IsSetCard(SET_HERO) and c:IsDiscardable() end
 
 function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
-    if chk == 0 then return c:IsAbleToHandAsCost() end
-    Duel.SendtoHand(c, nil, REASON_COST)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_HAND, 0, 1, c) end
+    Duel.DiscardHand(tp, s.e2filter, 1, 1, REASON_COST + REASON_DISCARD)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chk == 0 then
-        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-                   Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_HAND + LOCATION_GRAVE, 0, 1, nil, e, tp)
-    end
-
-    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND + LOCATION_GRAVE)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and c:IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
     if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
-
-    local g = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, aux.NecroValleyFilter(s.e2filter), tp, LOCATION_HAND + LOCATION_GRAVE, 0, 1, 1, nil, e,
-        tp)
-    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP) > 0 then
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetDescription(3300)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
+        ec1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+        ec1:SetValue(LOCATION_REMOVED)
+        ec1:SetReset(RESET_EVENT + RESETS_REDIRECT)
+        c:RegisterEffect(ec1, true)
+    end
 end
 
 function s.e3efffilter(c) return c:IsType(TYPE_FUSION) and c:IsSetCard(SET_ELEMENTAL_HERO) end
@@ -85,11 +87,11 @@ function s.e3effop(e, tp, eg, ep, ev, re, r, rp)
         tc:RegisterEffect(ec0, true)
     end
 
-    -- attack all monsters
+    -- atk up
     local ec1 = Effect.CreateEffect(c)
     ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetCode(EFFECT_ATTACK_ALL)
-    ec1:SetValue(1)
+    ec1:SetCode(EFFECT_UPDATE_ATTACK)
+    ec1:SetValue(400)
     ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
     tc:RegisterEffect(ec1, true)
 end
