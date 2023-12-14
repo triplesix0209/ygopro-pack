af731821292183b29dd256c52339edbddde6bca7
Luc THQ <triplesix0209@gmail.com>
2023-12-14 12:06:56 +0700

    up

--- pack/red-eyes/script/red-eyes-darkness-flare-metal-dragon.lua
+++ pack/red-eyes/script/red-eyes-darkness-flare-metal-dragon.lua
@@ -21,20 +21,17 @@ function s.initial_effect(c)
     e1:SetValue(1)
     c:RegisterEffect(e1)
 
-    -- negate
+    -- special summon 1 Normal Monster from the GY
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
-    e2:SetCategory(CATEGORY_NEGATE + CATEGORY_DESTROY)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
-    e2:SetCode(EVENT_CHAINING)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.e2con)
-    e2:SetCost(s.e2cost)
+    e2:SetCountLimit(1, id)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
-    c:RegisterEffect(e2, false, REGISTER_FLAG_DETACH_XMAT)
+    c:RegisterEffect(e2)
 
     -- atk up
     local e3 = Effect.CreateEffect(c)
@@ -46,50 +43,87 @@ function s.initial_effect(c)
     e3:SetValue(function(e, c) return c:GetOverlayCount() * 300 end)
     c:RegisterEffect(e3)
 
-    -- attach
+    -- negate
     local e4 = Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F)
-    e4:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
-    e4:SetCode(EVENT_DESTROYED)
+    e4:SetDescription(aux.Stringid(id, 2))
+    e4:SetCategory(CATEGORY_NEGATE)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
+    e4:SetCode(EVENT_CHAINING)
     e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
     e4:SetCondition(s.e4con)
+    e4:SetCost(s.e4cost)
     e4:SetTarget(s.e4tg)
     e4:SetOperation(s.e4op)
-    c:RegisterEffect(e4)
+    c:RegisterEffect(e4, false, REGISTER_FLAG_DETACH_XMAT)
+
+    -- attach
+    local e5 = Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F)
+    e5:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCondition(s.e5con)
+    e5:SetTarget(s.e5tg)
+    e5:SetOperation(s.e5op)
+    c:RegisterEffect(e5)
 end
 
 function s.xyzovfilter(c, tp, sc)
     return c:IsFaceup() and c:IsSetCard(SET_RED_EYES, sc, SUMMON_TYPE_XYZ, tp) and c:IsType(TYPE_XYZ, sc, SUMMON_TYPE_XYZ, tp) and c:GetRank() == 7
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp) return ep ~= tp and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) end
+function s.e2filter(c, e, tp) return c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false) end
 
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and Duel.IsExistingTarget(s.e2filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectTarget(tp, s.e2filter, tp, LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, #g, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP) end
+end
+
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return ep ~= tp and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and
+               c:GetOverlayGroup():IsExists(Card.IsCode, 1, nil, 44405066)
+end
+
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return e:GetHandler():CheckRemoveOverlayCard(tp, 1, REASON_COST) end
     e:GetHandler():RemoveOverlayCard(tp, 1, 1, REASON_COST)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return true end
     Duel.SetOperationInfo(0, CATEGORY_NEGATE, eg, #eg, 0, 0)
-    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then Duel.SetOperationInfo(0, CATEGORY_DESTROY, eg, #eg, 0, 0) end
 end
-
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
-    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then Duel.Destroy(eg, REASON_EFFECT) end
-end
-
-function s.e4filter(c, tp, rc, re) return c:IsReason(REASON_BATTLE + REASON_EFFECT) and (c:GetReasonCard() == rc or (re and re:GetOwner() == rc)) end
-
-function s.e4con(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    return c:GetOverlayGroup():IsExists(Card.IsCode, 1, nil, 44405066) and eg:IsExists(s.e4filter, 1, nil, tp, c, re)
-end
-
-function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk) if chk == 0 then return e:GetHandler():IsType(TYPE_XYZ) end end
 
 function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local g = eg:Filter(s.e4filter, nil, tp, c, re)
+    local rc = re:GetHandler()
+    if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then Duel.Overlay(c, rc) end
+end
+
+function s.e5filter(c, tp, rc, re) return c:IsReason(REASON_BATTLE + REASON_EFFECT) and (c:GetReasonCard() == rc or (re and re:GetOwner() == rc)) end
+
+function s.e5con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return c:GetOverlayGroup():IsExists(Card.IsCode, 1, nil, 44405066) and eg:IsExists(s.e5filter, 1, nil, tp, c, re)
+end
+
+function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk) if chk == 0 then return e:GetHandler():IsType(TYPE_XYZ) end end
+
+function s.e5op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local g = eg:Filter(s.e5filter, nil, tp, c, re)
     if c:IsType(TYPE_XYZ) and #g > 0 then Duel.Overlay(c, g) end
 end
