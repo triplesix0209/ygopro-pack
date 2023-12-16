cc126ed975b8adfc407c3c9d68c31e946d63e9c1
Luc THQ <triplesix0209@gmail.com>
2023-12-16 17:38:23 +0700

    up

---
+++ pack/elemental-neos/script/neo-spacian-blaze-scarab.lua
@@ -0,0 +1,22 @@
+-- Neo-Spacian Blaze Scarab
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- cannot special summon
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    ec1:SetCode(EFFECT_SPSUMMON_CONDITION)
+    c:RegisterEffect(ec1)
+
+    -- add code
+    local ec2 = Effect.CreateEffect(c)
+    ec2:SetType(EFFECT_TYPE_SINGLE)
+    ec2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    ec2:SetCode(EFFECT_ADD_CODE)
+    ec2:SetValue(89621922)
+    c:RegisterEffect(ec2)
+end
