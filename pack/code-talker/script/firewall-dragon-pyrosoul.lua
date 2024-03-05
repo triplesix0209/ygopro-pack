02055f4507a9a56e9494f7d3cafc5a6a8368d2ab
Luc THQ <triplesix0209@gmail.com>
2024-03-05 16:26:39 +0700

    up

--- pack/code-talker/script/firewall-dragon-pyrosoul.lua
+++ pack/code-talker/script/firewall-dragon-pyrosoul.lua
@@ -4,8 +4,19 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
+    c:SetUniqueOnField(1, 0, id)
 
     -- link summon
     Link.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsRace, RACE_CYBERSE), 2, 99,
         function(g, sc, sumtype, tp) return g:CheckDifferentPropertyBinary(Card.GetAttribute, sc, sumtype, tp) end)
+
+    -- indes
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
+    e1:SetTarget(function(e, c) return c:IsFaceup() and (c == e:GetHandler() or e:GetHandler():GetLinkedGroup():IsContains(c)) end)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
 end
