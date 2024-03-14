6a7b35143c8952cac01e8a66456e44df76e3de82
Luc THQ <triplesix0209@gmail.com>
2024-03-14 20:37:24 +0700

    up

--- pack/code-talker/script/shootingcode-talker-oceanspirit.lua
+++ pack/code-talker/script/shootingcode-talker-oceanspirit.lua
@@ -23,7 +23,6 @@ function s.initial_effect(c)
     e2:SetDescription(aux.Stringid(id, 0))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_MZONE)
     e2:SetHintTiming(0, TIMING_END_PHASE)
@@ -39,7 +38,7 @@ function s.e1val(e, c)
     return g:GetClassCount(Card.GetAttribute) * 500
 end
 
-function s.e2filter1(c) return c:IsSetCard(SET_CYNET) and c:IsSpellTrap() and c:IsSSetable() end
+function s.e2filter1(c) return c:IsSetCard(SET_CYNET) and c:IsSpellTrap() and c:IsAbleToHand() end
 
 function s.e2filter2(c, e, tp)
     return not c:IsCode(id) and c:IsRace(RACE_CYBERSE) and c:IsLinkBelow(3) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
@@ -51,14 +50,17 @@ function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
 end
 
 function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.e2filter1, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil) end
-    Duel.SetOperationInfo(0, CATEGORY_LEAVE_GRAVE, nil, 1, 0, 0)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.e2filter1, tp, LOCATION_GRAVE, 0, 1, nil) end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, 0, LOCATION_GRAVE)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local g = Utility.SelectMatchingCard(HINTMSG_SET, tp, s.e2filter1, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
-    if #g > 0 and Duel.SSet(tp, g) > 0 and Duel.GetLP(tp) <= 2000 and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, s.e2filter1, tp, LOCATION_GRAVE, 0, 1, 1, nil)
+    if #g == 0 or Duel.SendtoHand(g, nil, REASON_EFFECT) == 0 then return end
+
+    Duel.ConfirmCards(1 - tp, g)
+    if Duel.GetLP(tp) <= 2000 and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
         Duel.IsExistingMatchingCard(s.e2filter2, tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, nil, e, tp) and
         Duel.SelectEffectYesNo(tp, c, aux.Stringid(id, 1)) then
         Duel.BreakEffect()
