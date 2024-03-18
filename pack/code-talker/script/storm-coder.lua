bae1fa236c093702992ece96bb09016c775c6e37
Luc THQ <triplesix0209@gmail.com>
2024-03-19 05:08:07 +0700

    up

--- pack/code-talker/script/storm-coder.lua
+++ pack/code-talker/script/storm-coder.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCountLimit(1, id)
+    e1:SetCountLimit(1, {id, 1})
     e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
@@ -39,6 +39,18 @@ function s.initial_effect(c)
     e2b:SetLabelObject(e2)
     c:RegisterEffect(e2b)
     aux.GlobalCheck(s, function() s.flagmap = {} end)
+
+    -- draw
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_DRAW)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_BE_MATERIAL)
+    e3:SetCountLimit(1, {id, 2})
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e1filter(c) return c:ListsArchetype(SET_CODE_TALKER) and c:IsMonster() and c:IsAbleToHand() end
@@ -88,4 +100,18 @@ function s.e2val(chk, summon_type, e, ...)
             s.flagmap[c] = nil
         end
     end
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsLocation(LOCATION_GRAVE) and r == REASON_LINK end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsPlayerCanDraw(tp, 1) end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER, CHAININFO_TARGET_PARAM)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
