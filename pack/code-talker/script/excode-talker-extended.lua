0c1c437296b225b0d19335da344a990cbe5b14dd
Luc THQ <triplesix0209@gmail.com>
2024-03-25 10:27:34 +0700

    up

--- pack/code-talker/script/excode-talker-extended.lua
+++ pack/code-talker/script/excode-talker-extended.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
     e1:SetValue(40669071)
     c:RegisterEffect(e1)
 
-    -- atk up & indes
+    -- atk up & protect
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetCode(EFFECT_UPDATE_ATTACK)
@@ -29,10 +29,19 @@ function s.initial_effect(c)
     e2:SetValue(500)
     c:RegisterEffect(e2)
     local e2b = e2:Clone()
-    e2b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e2b:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+    e2b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e2b:SetValue(aux.indoval)
     c:RegisterEffect(e2b)
+    local e2c = e2:Clone()
+    e2c:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2c:SetCode(EFFECT_CANNOT_REMOVE)
+    e2c:SetTargetRange(1, 1)
+    e2c:SetTarget(function(e, c, rp, r, re)
+        local tp = e:GetHandlerPlayer()
+        return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and rp == 1 - tp and r & REASON_EFFECT ~= 0
+    end)
+    c:RegisterEffect(e2c)
 
     -- negate effect
     local e3 = Effect.CreateEffect(c)
