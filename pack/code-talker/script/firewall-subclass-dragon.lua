aa0e0cbe3c02de8cb04f6c5bb3ea9e6c419a971a
Luc THQ <triplesix0209@gmail.com>
2024-03-25 07:41:57 +0700

    up

--- pack/code-talker/script/firewall-subclass-dragon.lua
+++ pack/code-talker/script/firewall-subclass-dragon.lua
@@ -4,4 +4,8 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- link summon
+    Link.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsType, TYPE_EFFECT), 2, 99,
+        function(g, sc, sumtype, tp) return g:IsExists(Card.IsType, 1, nil, TYPE_LINK, sc, sumtype, tp) end)
 end
