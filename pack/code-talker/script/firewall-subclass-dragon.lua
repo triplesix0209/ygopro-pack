f4b727ccfcb73250c7484a39198a132e19225f74
Luc THQ <triplesix0209@gmail.com>
2024-03-25 07:47:43 +0700

    up

--- pack/code-talker/script/firewall-subclass-dragon.lua
+++ pack/code-talker/script/firewall-subclass-dragon.lua
@@ -6,5 +6,7 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
 
     -- link summon
-    Link.AddProcedure(c, nil, 2, 99, function(g, sc, sumtype, tp) return g:IsExists(function(c) return c:GetLink() == 4 end, 1, nil) end)
+    Link.AddProcedure(c, nil, 2, 99, function(g, sc, sumtype, tp)
+        return g:IsExists(function(c) return c:GetLink() == 4 and c:IsRace(RACE_CYBERSE, sc, sumtype, tp) end, 1, nil, sc, sumtype, tp)
+    end)
 end
