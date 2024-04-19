d6d533c6792af421fe3bc7dc1e8e0481e34fe222
Luc THQ <triplesix0209@gmail.com>
2024-04-19 10:31:01 +0700

    up

--- pack/numeron/script/icxyz-barian-hope-emperor.lua
+++ pack/numeron/script/icxyz-barian-hope-emperor.lua
@@ -59,13 +59,13 @@ function s.initial_effect(c)
     nopos:SetRange(LOCATION_MZONE)
     c:RegisterEffect(nopos)
 
-    -- gain effect "barian hope"
+    -- gain effects
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
     e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetCondition(s.e1con)
-    e1:SetOperation(s.e1op)
+    e1:SetOperation(s.e1op1)
     c:RegisterEffect(e1)
     local e1matcheck = Effect.CreateEffect(c)
     e1matcheck:SetType(EFFECT_TYPE_SINGLE)
@@ -73,35 +73,55 @@ function s.initial_effect(c)
     e1matcheck:SetValue(s.e1matcheck)
     e1matcheck:SetLabelObject(e1)
     c:RegisterEffect(e1matcheck)
+    local e1b = e1:Clone()
+    e1b:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e1b:SetCode(EVENT_ADJUST)
+    e1b:SetRange(LOCATION_MZONE)
+    e1b:SetOperation(s.e1op2)
+    c:RegisterEffect(e1b)
 
-    -- cards cannot be negated
+    -- immune
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_DISEFFECT)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCondition(s.effcon)
-    e2:SetValue(function(e, ct)
+    e2:SetValue(aux.tgoval)
+    c:RegisterEffect(e2)
+    local e2b = e1:Clone()
+    e2b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2b:SetValue(1)
+    c:RegisterEffect(e2b)
+    local e2c = Effect.CreateEffect(c)
+    e2c:SetType(EFFECT_TYPE_FIELD)
+    e2c:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2c:SetCode(EFFECT_CANNOT_REMOVE)
+    e2c:SetRange(LOCATION_MZONE)
+    e2c:SetTargetRange(1, 1)
+    e2c:SetCondition(s.effcon)
+    e2c:SetTarget(function(e, c, tp, r) return c == e:GetHandler() and r == REASON_EFFECT end)
+    c:RegisterEffect(e2c)
+
+    -- cards cannot be negated
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_DISEFFECT)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCondition(s.effcon)
+    e3:SetValue(function(e, ct)
         local c = e:GetHandler()
         local p = c:GetControler()
         local te, tp, loc = Duel.GetChainInfo(ct, CHAININFO_TRIGGERING_EFFECT, CHAININFO_TRIGGERING_PLAYER, CHAININFO_TRIGGERING_LOCATION)
         if p ~= tp or (loc & LOCATION_ONFIELD) == 0 then return false end
         return te:GetHandler() == c or te:GetHandler():IsSetCard({SET_NUMBER_C, SET_BARIANS, SET_SEVENTH})
     end)
-    c:RegisterEffect(e2)
-    local e2b = e2:Clone()
-    e2b:SetCode(EFFECT_CANNOT_DISABLE)
-    e2b:SetTargetRange(LOCATION_ONFIELD, 0)
-    e2b:SetTarget(function(e, tc) return tc == e:GetHandler() or tc:IsSetCard({SET_NUMBER_C, SET_BARIANS, SET_SEVENTH}) end)
-    c:RegisterEffect(e2b)
-
-    -- gain effect "number C101 - C107"
-    local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-    e3:SetCode(EVENT_ADJUST)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCondition(s.effcon)
-    e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetCode(EFFECT_CANNOT_DISABLE)
+    e3b:SetTargetRange(LOCATION_ONFIELD, 0)
+    e3b:SetTarget(function(e, tc) return tc == e:GetHandler() or tc:IsSetCard({SET_NUMBER_C, SET_BARIANS, SET_SEVENTH}) end)
+    c:RegisterEffect(e3b)
 
     -- detach replace
     local e4 = Effect.CreateEffect(c)
@@ -118,26 +138,24 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and #e:GetLabelObject() > 0 end
-
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    local tc = e:GetLabelObject():GetFirst()
-    c:CopyEffect(tc:GetOriginalCode(), RESET_EVENT | RESETS_STANDARD)
-end
-
 function s.e1matcheck(e, c)
     local g = c:GetMaterial():Filter(Card.IsCode, nil, 67926903)
     e:GetLabelObject():SetLabelObject(g)
 end
 
-function s.effcon(e) return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode, 1, nil, 67926903) end
+function s.e1filter(c) return c:IsType(TYPE_XYZ) and c:IsSetCard(SET_NUMBER_C) and c.xyz_number and c.xyz_number >= 101 and c.xyz_number <= 107 end
 
-function s.e3filter(c) return c:IsType(TYPE_XYZ) and c:IsSetCard(SET_NUMBER_C) and c.xyz_number and c.xyz_number >= 101 and c.xyz_number <= 107 end
+function s.e1con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and #e:GetLabelObject() > 0 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e1op1(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local og = c:GetOverlayGroup():Filter(s.e3filter, nil)
+    local tc = e:GetLabelObject():GetFirst()
+    c:CopyEffect(tc:GetOriginalCode(), RESET_EVENT | RESETS_STANDARD)
+end
+
+function s.e1op2(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local og = c:GetOverlayGroup():Filter(s.e1filter, nil)
     local g = og:Filter(function(c) return c:GetFlagEffect(id) == 0 end, nil)
     if #g <= 0 then return end
 
@@ -167,7 +185,7 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
             reset:SetReset(RESET_EVENT + RESETS_STANDARD)
             c:RegisterEffect(reset, true)
 
-            local original_num_code = s.e3numcode(tc)
+            local original_num_code = s.e1numcode(tc)
             if (original_num_code) then
                 local ec1 = Effect.CreateEffect(c)
                 ec1:SetType(EFFECT_TYPE_SINGLE)
@@ -180,7 +198,7 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e3numcode(c)
+function s.e1numcode(c)
     if c:IsCode({12744567, 900004003}) then return 48739166 end
     if c:IsCode(67173574) then return 49678559 end
     if c:IsCode(20785975) then return 94380860 end
@@ -189,3 +207,5 @@ function s.e3numcode(c)
     if c:IsCode(55888045) then return 63746411 end
     if c:IsCode(68396121) then return 88177324 end
 end
+
+function s.effcon(e) return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode, 1, nil, 67926903) end
