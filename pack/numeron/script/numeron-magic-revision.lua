1491e697e0dec250892f7650bd81deec4dcc729b
lucthq-giganteast <lucthq@giganteast.com>
2024-04-21 18:20:21 +0700

    up

--- pack/numeron/script/numeron-magic-revision.lua
+++ pack/numeron/script/numeron-magic-revision.lua
@@ -5,7 +5,7 @@ local s, id = GetID()
 s.listed_names = {CARD_NUMERON_NETWORK}
 
 function s.initial_effect(c)
-    -- Activate
+    -- activate
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_TOGRAVE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -17,35 +17,76 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
-function s.e1filter(c, tc)
-    return not c:IsCode(tc:GetCode()) and not c:IsPublic() and c:IsSpell() and c:IsAbleToGrave() and c:CheckActivateEffect(false, true, true) ~= nil
+function s.e1filter(c, e, tp, chain)
+    if c:IsCode(e:GetHandler():GetCode()) or c:IsPublic() or not c:IsAbleToGrave() or not (c:IsNormalSpell() or c:IsQuickPlaySpell()) then
+        return false
+    end
+    local te = c:GetActivateEffect()
+    if te == nil then return false end
+
+    local eg, ep, ev, re, r, rp
+    local condition = te:GetCondition()
+    local target = te:GetTarget()
+
+    if te:GetCode() == EVENT_CHAINING then
+        if chain <= 0 then return false end
+        local te2, p = Duel.GetChainInfo(chain, CHAININFO_TRIGGERING_EFFECT, CHAININFO_TRIGGERING_PLAYER)
+        local tc = te2:GetHandler()
+        local g = Group.FromCards(tc)
+        eg, ep, ev, re, r, rp = g, p, chain, te2, REASON_EFFECT, p
+        return (not condition or condition(e, tp, eg, ep, ev, re, r, rp)) and (not target or target(e, tp, eg, ep, ev, re, r, rp, 0))
+    elseif te:GetCode() == EVENT_FREE_CHAIN then
+        return (not condition or condition(e, tp, eg, ep, ev, re, r, rp)) and (not target or target(e, tp, eg, ep, ev, re, r, rp, 0))
+    else
+        local res, teg, tep, tev, tre, tr, trp = Duel.CheckEvent(te:GetCode(), true)
+        return res and (not condition or condition(e, tp, teg, tep, tev, tre, tr, trp)) and
+                   (not target or target(e, tp, teg, tep, tev, tre, tr, trp, 0))
+    end
 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
+    local ec = re:GetHandler()
     return not Duel.IsExistingMatchingCard(function(c) return c:IsSpellTrap() and not c:IsCode(CARD_NUMERON_NETWORK) end, tp, LOCATION_ONFIELD, 0, 1,
-        c) and rp == 1 - tp and re:IsSpellEffect()
+        c) and rp == 1 - tp and (ec:IsNormalSpell() or ec:IsQuickPlaySpell())
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local ec = re:GetHandler()
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.e1filter, tp, 0, LOCATION_DECK, 1, nil, ec) end
+    local chain = Duel.GetCurrentChain()
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.e1filter, tp, 0, LOCATION_DECK, 1, nil, re, rp, chain) end
 
-    Duel.SetOperationInfo(0, CATEGORY_TOGRAVE, nil, 1, 1 - tp, LOCATION_DECK)
-end
-
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local ec = re:GetHandler()
-    local tc = Utility.SelectMatchingCard(HINTMSG_CONFIRM, tp, s.e1filter, tp, 0, LOCATION_DECK, 1, 1, nil, ec):GetFirst()
-    if not tc then return end
+    chain = chain - 1;
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_CONFIRM)
+    local tc = Duel.SelectMatchingCard(tp, s.e1filter, tp, 0, LOCATION_DECK, 1, 1, nil, re, rp, chain):GetFirst()
     Duel.ConfirmCards(1 - tp, tc)
 
     Utility.HintCard(tc)
-    Duel.ChangeChainOperation(ev, aux.FALSE)
-    local te, ceg, cep, cev, cre, cr, crp = tc:CheckActivateEffect(false, true, true)
-    local tg = te:GetTarget()
-    if tg then tg(e, tp, ceg, cep, cev, cre, cr, crp, 1) end
-    local op = te:GetOperation()
-    if op then op(e, tp, eg, ep, ev, re, r, rp) end
-    Duel.SendtoGrave(tc, REASON_EFFECT)
+    local te = tc:GetActivateEffect()
+    local teg, tep, tev, tre, tr, trp
+    if te:GetCode() == EVENT_CHAINING then
+        if chain <= 0 then return false end
+        local te2, p = Duel.GetChainInfo(chain, CHAININFO_TRIGGERING_EFFECT, CHAININFO_TRIGGERING_PLAYER)
+        local tc = te2:GetHandler()
+        local g = Group.FromCards(tc)
+        teg, tep, tev, tre, tr, trp = g, p, chain, te2, REASON_EFFECT, p
+    end
+    e:SetLabelObject(te)
+    local target = te:GetTarget()
+    e:SetProperty(te:GetProperty())
+    if target then target(re, tp, teg, tep, tev, tre, tr, trp, 1) end
+    Duel.ClearOperationInfo(0)
+
+    Duel.SetOperationInfo(0, CATEGORY_TOGRAVE, tc, 1, 0, 0)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local te = e:GetLabelObject()
+    if not te then return end
+    if Duel.SendtoGrave(te:GetHandler(), REASON_EFFECT) == 0 then return end
+
+    local operation = te:GetOperation()
+    if operation then
+        Duel.ChangeChainOperation(ev, aux.FALSE)
+        operation(e, rp, eg, ep, ev, re, r, rp)
+    end
 end
