b04ed024effc62f2ec3183d70f8acda172c0b00a
Luc THQ <triplesix0209@gmail.com>
2024-05-18 19:45:58 +0700

    up

--- pack/dragon-ruler/script/dragon-deity-wind.lua
+++ pack/dragon-ruler/script/dragon-deity-wind.lua
@@ -5,4 +5,14 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     DragonRuler.RegisterEmperorEffect(s, c, id, ATTRIBUTE_WIND)
+
+    -- spell immune
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_IMMUNE_EFFECT)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE, 0)
+    e1:SetTarget(function(e, c) return c == e:GetHandler() or (c:IsRace(RACE_DRAGON) and c:GetMutualLinkedGroupCount() > 0) end)
+    e1:SetValue(function(e, te) return te:IsActiveType(TYPE_SPELL) and te:GetOwnerPlayer() ~= e:GetHandlerPlayer() end)
+    c:RegisterEffect(e1)
 end
