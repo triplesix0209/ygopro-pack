97b86965038988380621c746beba90580536ac90
lucthq-giganteast <lucthq@giganteast.com>
2024-05-18 14:38:55 +0700

    up

--- pack/dragon-ruler/script/mesaia-genesis.lua
+++ pack/dragon-ruler/script/mesaia-genesis.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util_dragon_ruler.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    Pendulum.AddProcedure(c)
+    c:EnableReviveLimit()
     Pendulum.AddProcedure(c, false)
 
     -- link summon
