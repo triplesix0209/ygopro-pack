65dd2a414e659060b9449cc73a0ba9118869aa61
Luc THQ <triplesix0209@gmail.com>
2024-05-22 10:24:59 +0700

    up

--- pack/dragon-ruler/script/dragon-deity-water.lua
+++ pack/dragon-ruler/script/dragon-deity-water.lua
@@ -6,7 +6,7 @@ local s, id = GetID()
 function s.initial_effect(c)
     DragonRuler.RegisterDeityEffect(s, c, id, ATTRIBUTE_WATER)
 
-    -- indes
+    -- indes & avoid damage
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -14,7 +14,10 @@ function s.initial_effect(c)
     e1:SetTargetRange(LOCATION_MZONE, 0)
     e1:SetTarget(function(e, c) return c == e:GetHandler() or (c:GetMutualLinkedGroupCount() > 0 and c:IsLinkAbove(5) and c:IsRace(RACE_DRAGON)) end)
     e1:SetValue(1)
-    c:RegisterEffect(e1)
+    c:RegisterEffect(e1)    
+    local e1b = e1:Clone()
+    e1b:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    c:RegisterEffect(e1b)
 
     -- block & steal atk
     local e2 = Effect.CreateEffect(c)
