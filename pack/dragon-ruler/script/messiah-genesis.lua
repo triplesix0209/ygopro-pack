c81d46c993563be3c4f7457b258be585f650ae75
lucthq-giganteast <lucthq@giganteast.com>
2024-05-26 13:10:53 +0700

    up

--- pack/dragon-ruler/script/messiah-genesis.lua
+++ pack/dragon-ruler/script/messiah-genesis.lua
@@ -1,8 +1,22 @@
--- Messiah, Genesis of Dragons
+-- Messiah, Genesis Deity of Dragons
 Duel.LoadScript("util.lua")
 Duel.LoadScript("util_dragon_ruler.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
     DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_WATER + ATTRIBUTE_EARTH + ATTRIBUTE_DARK, LOCATION_DECK + LOCATION_GRAVE)
+
+    -- act limit
+    local pe1 = Effect.CreateEffect(c)
+    pe1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    pe1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    pe1:SetRange(LOCATION_PZONE)
+    pe1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
+        if eg:IsExists(s.pe1filter, 1, nil, tp) then Duel.SetChainLimitTillChainEnd(s.pe1chainlimit) end
+    end)
+    c:RegisterEffect(pe1)
 end
+
+function s.pe1filter(c, tp) return c:IsRace(RACE_DRAGON) and c:IsControler(tp) and c:IsSummonType(SUMMON_TYPE_PENDULUM) end
+
+function s.pe1chainlimit(e, rp, tp) return tp == rp or (e:IsActiveType(TYPE_SPELL + TYPE_TRAP) and not e:IsHasType(EFFECT_TYPE_ACTIVATE)) end
--- pack/dragon-ruler/script/messiah-origin.lua
+++ pack/dragon-ruler/script/messiah-origin.lua
@@ -1,8 +1,18 @@
--- Messiah, Origin of Dragons
+-- Messiah, Origin Deity of Dragons
 Duel.LoadScript("util.lua")
 Duel.LoadScript("util_dragon_ruler.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
     DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_FIRE + ATTRIBUTE_WIND + ATTRIBUTE_LIGHT, LOCATION_HAND + LOCATION_DECK)
+
+    -- cannot disable pendulum summon
+    local pe1 = Effect.CreateEffect(c)
+    pe1:SetType(EFFECT_TYPE_FIELD)
+    pe1:SetProperty(EFFECT_FLAG_IGNORE_RANGE + EFFECT_FLAG_SET_AVAILABLE)
+    pe1:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+    pe1:SetRange(LOCATION_PZONE)
+    pe1:SetTargetRange(1, 0)
+    pe1:SetTarget(function(e, c) return c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsRace(RACE_DRAGON) end)
+    c:RegisterEffect(pe1)
 end
