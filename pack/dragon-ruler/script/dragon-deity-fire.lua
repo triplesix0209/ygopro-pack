718c9ba7ec63590628de9d9a7de0295bd1aaef6d
Luc THQ <triplesix0209@gmail.com>
2024-05-26 17:41:11 +0700

    up

--- pack/dragon-ruler/script/dragon-deity-fire.lua
+++ pack/dragon-ruler/script/dragon-deity-fire.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
     -- indes & unstoppable attack
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e1:SetRange(LOCATION_MZONE)
     e1:SetTargetRange(LOCATION_MZONE, 0)
--- pack/dragon-ruler/script/dragon-deity-water.lua
+++ pack/dragon-ruler/script/dragon-deity-water.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
     -- indes & avoid damage
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e1:SetRange(LOCATION_MZONE)
     e1:SetTargetRange(LOCATION_MZONE, 0)
