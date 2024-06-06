2ec4dc4196fa9d85726d09f2a6272dbd94661e6b
Luc THQ <triplesix0209@gmail.com>
2024-06-06 23:01:09 +0700

    up

--- pack/dragon-ruler/script/dragon-elysium.lua
+++ pack/dragon-ruler/script/dragon-elysium.lua
@@ -3,5 +3,9 @@ Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    -- activate
+    local act = Effect.CreateEffect(c)
+    act:SetType(EFFECT_TYPE_ACTIVATE)
+    act:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(act)
 end
