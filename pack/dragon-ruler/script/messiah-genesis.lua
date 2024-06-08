8e278d317fff0accd0b30e502f3d2f4e7df469fd
Luc THQ <triplesix0209@gmail.com>
2024-06-08 18:45:02 +0700

    up

--- pack/dragon-ruler/script/messiah-genesis.lua
+++ pack/dragon-ruler/script/messiah-genesis.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util_dragon_ruler.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_WATER + ATTRIBUTE_EARTH + ATTRIBUTE_DARK, LOCATION_DECK + LOCATION_GRAVE)
+    DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_FIRE + ATTRIBUTE_EARTH + ATTRIBUTE_DARK, LOCATION_DECK + LOCATION_GRAVE)
 
     -- act limit
     local pe1 = Effect.CreateEffect(c)
--- pack/dragon-ruler/script/messiah-origin.lua
+++ pack/dragon-ruler/script/messiah-origin.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util_dragon_ruler.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_FIRE + ATTRIBUTE_WIND + ATTRIBUTE_LIGHT, LOCATION_HAND + LOCATION_DECK)
+    DragonRuler.RegisterMessiahBabyEffect(s, c, id, ATTRIBUTE_WATER + ATTRIBUTE_WIND + ATTRIBUTE_LIGHT, LOCATION_HAND + LOCATION_DECK)
 
     -- cannot disable pendulum summon
     local pe1 = Effect.CreateEffect(c)
