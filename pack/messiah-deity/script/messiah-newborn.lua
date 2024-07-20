b236afd64bcab56ccf58e66fc732eeba2a88dcf3
Luc THQ <triplesix0209@gmail.com>
2024-07-20 18:55:15 +0700

    up

--- pack/messiah-deity/script/messiah-newborn.lua
+++ pack/messiah-deity/script/messiah-newborn.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util_messiah.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    DragonRuler.RegisterMessiahBabyEffect(s, c, id, LOCATION_EXTRA + LOCATION_DECK, true)
+    DragonRuler.RegisterMessiahBabyEffect(s, c, id, LOCATION_DECK)
 
     -- link summon
     Link.AddProcedure(c, function(c, sc, sumtype, tp) return not c:IsType(TYPE_LINK, sc, sumtype, tp) end, 2, 3)
--- pack/messiah-deity/script/messiah-origin.lua
+++ pack/messiah-deity/script/messiah-origin.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util_messiah.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    DragonRuler.RegisterMessiahBabyEffect(s, c, id, LOCATION_HAND, false)
+    DragonRuler.RegisterMessiahBabyEffect(s, c, id, LOCATION_HAND)
 
     -- link summon
     Link.AddProcedure(c, function(c, sc, sumtype, tp) return not c:IsType(TYPE_LINK, sc, sumtype, tp) end, 2, 2)
