0c17291310fc20d914dfc6743fb99354e29069ff
Luc THQ <triplesix0209@gmail.com>
2024-07-20 23:43:13 +0700

    up

--- pack/messiah-deity/script/messiah-genesis.lua
+++ pack/messiah-deity/script/messiah-genesis.lua
@@ -8,4 +8,22 @@ function s.initial_effect(c)
 
     -- link summon
     Link.AddProcedure(c, function(c, sc, sumtype, tp) return not c:IsType(TYPE_LINK, sc, sumtype, tp) end, 2, 2)
+
+    -- destroy itself
+    local me1 = Effect.CreateEffect(c)
+    me1:SetDescription(aux.Stringid(id, 3))
+    me1:SetCategory(CATEGORY_DESTROY)
+    me1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F)
+    me1:SetRange(LOCATION_MZONE)
+    me1:SetCode(EVENT_PHASE + PHASE_END)
+    me1:SetCountLimit(1)
+    me1:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk)
+        if chk == 0 then return true end
+        Duel.SetOperationInfo(0, CATEGORY_DESTROY, e:GetHandler(), 1, 0, 0)
+    end)
+    me1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
+        local c = e:GetHandler()
+        if c:IsRelateToEffect(e) and c:IsFaceup() then Duel.Destroy(c, REASON_EFFECT) end
+    end)
+    c:RegisterEffect(me1)
 end
--- pack/messiah-deity/script/messiah-newborn.lua
+++ pack/messiah-deity/script/messiah-newborn.lua
@@ -8,4 +8,14 @@ function s.initial_effect(c)
 
     -- link summon
     Link.AddProcedure(c, function(c, sc, sumtype, tp) return not c:IsType(TYPE_LINK, sc, sumtype, tp) end, 2, 3)
+
+    -- indes & avoid battle damage
+    local me1 = Effect.CreateEffect(c)
+    me1:SetType(EFFECT_TYPE_SINGLE)
+    me1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    me1:SetValue(1)
+    c:RegisterEffect(me1)
+    local me1b = me1:Clone()
+    me1b:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    c:RegisterEffect(me1b)
 end
