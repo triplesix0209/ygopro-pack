deaa31ff9bcd8941a354e3a564a7b8817afd7ebe
lucthq-giganteast <lucthq@giganteast.com>
2024-08-18 14:16:38 +0700

    up

--- pack/evil-hero/script/supreme-king-aura.lua
+++ pack/evil-hero/script/supreme-king-aura.lua
@@ -6,17 +6,25 @@ s.listed_names = {CARD_DARK_FUSION}
 s.listed_series = {0xf8}
 
 function s.initial_effect(c)
+    c:SetUniqueOnField(1, 0, id)
     aux.AddEquipProcedure(c, nil,
         function(c) return (c:GetOriginalLevel() == 12 and c:IsSetCard(0xf8)) or (c:IsType(TYPE_FUSION) and c.dark_calling) end)
 
-    -- immune
+    -- gain ATK & immune
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_EQUIP)
-    e1:SetCode(EFFECT_IMMUNE_EFFECT)
-    e1:SetValue(function(e, re)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(function(e, c)
+        local val = c:GetBaseDefense()
+        return val >= 0 and val or 0
+    end)
+    c:RegisterEffect(e1)
+    local e1b = e1:Clone()
+    e1b:SetCode(EFFECT_IMMUNE_EFFECT)
+    e1b:SetValue(function(e, re)
         local c = e:GetHandler()
         local rc = re:GetOwner()
         return rc ~= c and rc ~= c:GetEquipTarget()
     end)
-    c:RegisterEffect(e1)
+    c:RegisterEffect(e1b)
 end
